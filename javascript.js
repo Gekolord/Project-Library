f91f0e8684a6528b991836cc5e7d823dda50ce88
Gekolord <80892021+Gekolord@users.noreply.github.com>
2023-10-25 21:22:22 +0300

    finish working on submit button, updated delete button

--- javascript.js
+++ javascript.js
@@ -1,13 +1,39 @@
 const myLibrary = [];
 let content = document.getElementById('content');
 let bookButton = document.getElementById('new');
+let titleForm = document.getElementById('title');
+let authorForm = document.getElementById('author');
+let pagesForm = document.getElementById('pages');
+let isRead = document.getElementById('isRead');
+let submitBook = document.getElementById('submit');
+let wholeForm = document.getElementById('form');
+// deletes book from library and from screen
+function addDelete() {
+    let deleteButtons = document.querySelectorAll('.deleteButton')
+    for (const deleteButton of deleteButtons) {
+        index = deleteButton.parentElement.parentElement.dataset.indexValue
+        deleteButton.addEventListener('click', () => removeFromLibrary(index))
+        deleteButton.addEventListener('click', () => removeAllChildNodes(content))
+        deleteButton.addEventListener('click', () => display())
 
 
+    }
+}
 
-function Book(title, author, pages) {
+
+function Book(title, author, pages, isRead) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.isRead = isRead
+}
+// loops through library and assigns each book a data attribute in html
+function assignData() {
+    for (i in myLibrary) {
+
+        content.children[i].dataset.indexValue = i
+
+    } 
 }
 
 function removeFromLibrary(index) {
@@ -21,9 +47,17 @@ function removeAllChildNodes(parent) {
 }
 
 // creates book and adds it to library
-function addBookToLibrary(title, author, pages) {
-    const myBook = new Book(title, author, pages)
+function addBookToLibrary(title, author, pages, isRead) {
+    const myBook = new Book(title, author, pages, isRead)
     myLibrary.push(myBook)
+}
+// Checks if checkbox is checked and returns values depending on it
+function isChecked() {
+    if (isRead.checked) {
+        return 'read'
+    } else {
+        return 'not read'
+    }
 }
 
 // displays books on page
@@ -40,13 +74,15 @@ function display() {
             .textContent = myLibrary[i][j];
         }
         // selects first child and sets data attribute with index inside of an array in it
-        let firstChild = containerDiv.children[0];
-        let secondChild = containerDiv.children[1];
-        const index = myLibrary.findIndex(object => {
-            return object.title === `${firstChild.innerHTML}`
-             && object.author === `${secondChild.innerHTML}`;
-          });
-        firstChild.dataset.indexValue = `${index}`
+
+        // let firstChild = containerDiv.children[0];
+        // let secondChild = containerDiv.children[1];
+        // const index = myLibrary.findIndex(object => {
+        //     return object.title === `${firstChild.innerHTML}`
+        //      && object.author === `${secondChild.innerHTML}`;
+        //   });
+        // firstChild.dataset.indexValue = `${index}`
+        
         // Creates button container 
         let buttonContainer = document.createElement('div');
         buttonContainer.className = 'buttonContainer'
@@ -57,31 +93,35 @@ function display() {
         deleteButton.innerHTML = 'Delete book'
         buttonContainer.appendChild(deleteButton)
         // deletes book from library with click
-        deleteButton.addEventListener('click', () => removeFromLibrary(index))
+        
         deleteButton.addEventListener('click', () => deleteButton.parentElement.parentElement.remove())
         
 
     }
+    assignData();
+    addDelete()
 }
-addBookToLibrary('sos', 'ss', 123);
-addBookToLibrary('sos', 'ses', 123);
+addBookToLibrary('sos', 'ss', 123, 'read');
+addBookToLibrary('sos', 'ses', 123, 'read');
 display();
-const deleteButtons = document.querySelectorAll('.deleteButton')
 
-for (const deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', () => removeAllChildNodes(content))
-    deleteButton.addEventListener('click', () => display())
-
-}
 
 
 
 bookButton.addEventListener('click', function() {
-    const userInput = prompt('Please enter some text:');
-    console.log(userInput);
+    wholeForm.style.display = 'block'
 });
-
-
+// adds book to library, updates screen
+submitBook.addEventListener('click', function(event){
+    if (titleForm.value && authorForm.value && pagesForm.value) {
+        console.log(titleForm.value)
+        addBookToLibrary(titleForm.value, authorForm.value, pagesForm.value, isChecked());
+        event.preventDefault()
+        removeAllChildNodes(content)
+        display();
+        wholeForm.style.display = 'none'
+    }
+});
 
 
 
